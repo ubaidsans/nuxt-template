6af542ff197de22b8070ab66864c63bf3c983ef2
timotiusre21 <tiuz.fti@gmail.com>
2022-06-08 20:44:30 +0700

    update layout, tabs, dan image

---
+++ pages/about.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-card>
+    <v-tabs color="deep-purple accent-4" right>
+      <v-tab>Landscape</v-tab>
+      <v-tab>City</v-tab>
+      <v-tab>Abstract</v-tab>
+
+      <v-tab-item>
+        <v-container fluid>
+          <v-row>
+            <v-col cols="6" sm="4">
+              <v-img src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+                gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"></v-img>
+            </v-col>
+
+            <v-col cols="6" sm="4">
+              <v-img src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg">
+                <div class="fill-height bottom-gradient"></div>
+              </v-img>
+            </v-col>
+
+            <v-col cols="6" sm="4">
+              <v-img src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg">
+                <div class="fill-height repeating-gradient"></div>
+              </v-img>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-tab-item>
+
+      <v-tab-item>
+        <v-container fluid>
+          <v-row>
+            <v-col cols="6" sm="4">
+              <v-img src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+                gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"></v-img>
+            </v-col>
+
+            <v-col cols="6" sm="4">
+              <v-img src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg">
+                <div class="fill-height bottom-gradient"></div>
+              </v-img>
+            </v-col>
+
+            <v-col cols="6" sm="4">
+              <v-img src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg">
+                <div class="fill-height repeating-gradient"></div>
+              </v-img>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-tab-item>
+    </v-tabs>
+  </v-card>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,84 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-row no-gutters style="height: 300px;">
+      <v-col class="text-center" cols="12">
+        <v-avatar size="100px" class="mt-10">
+          <v-img src="/profile.jpg" max-height="150" max-width="250" />
+        </v-avatar>
+        <blockquote class="blockquote">
+          &#8220;Once you’ve accepted your flaws, no one can use them against you.&#8221;
+          <footer>
+            <small>
+              <em>Timotius &mdash; Business Analyst</em>
+            </small>
+          </footer>
+        </blockquote>
+        <v-spacer></v-spacer>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-tabs color="white accent-4" class="mt-20">
+            <v-tab>My Story</v-tab>
+            <v-tab>Personal Information</v-tab>
+            <v-tab>Abstract</v-tab>
+
+            <v-tab-item>
+              <v-container fluid>
+                <v-row>
+                  <v-text class="ml-5 mt-3 mb-3">
+                    Saya adalah seorang <i>technology enthusiast</i> yang selalu tertarik dengan hal yang baru dan
+                    menarik untuk dipelajari. <br />
+                    Hal-hal yang saya suka selain teknologi adalah <i>travelling around the world</i> khususnya negeri
+                    tercinta kita Indonesia Raya.<br />
+                    Google Cloud Platform adalah hal yang baru untuk saya, tetapi saya merasa tertantang untuk
+                    mempelajarinya. Terimakasih atas kesempatan ini.
+                  </v-text>
+                </v-row>
+              </v-container>
+            </v-tab-item>
+
+            <v-tab-item>
+              <v-container fluid>
+                <v-row>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="5">
+                        <v-text>
+                          <strong>Umur</strong>
+                        </v-text>
+                      </v-col>
+                      <v-col cols="2">
+                        <v-text>
+                          <strong>:</strong>
+                        </v-text>
+                      </v-col>
+                      <v-col cols="5">
+                        <v-text>
+                          <strong>30 th</strong>
+                        </v-text>
+                      </v-col>
+                    </v-row>
+                    <v-divider inset></v-divider>
+                  </v-container>
+                </v-row>
+              </v-container>
+            </v-tab-item>
+          </v-tabs>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'InspirePage'
 }
 </script>
+<style>
+div .v-main__wrap {
+  background-color: #0d5149;
+}
+</style>
